7b823a7a33cbe13077352099990f79478ea3e2e4
Jihanna02 <jihanna02@gmail.com>
2017-12-17 23:22:05 -0500

    psuedocoded, added inquirier to limit input

---
+++ keys.js
@@ -0,0 +1,8 @@
+var twitterKeys = {
+  consumer_key: 'jZCjH5grU2HVU6h3WWxFc8vfa',
+  consumer_secret: 'RG5p4ndrMA8umtRM9zXlX4lOdQZDsfaxiJNXktDn7Gdhn6Zdlh',
+  access_token_key: '2304454846-M5gZ9EsvDl11WAOFgsZhA9SrZavISoXx9zSDUvU',
+  access_token_secret: 'vEoMZj3UL1mEYn2DPe5P8DaKAheOgA2MNIvyi44wYjM1h',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,64 @@
+var keys = require('./keys.js');
+var inquirer = require('inquirer');
+
+inquirer.prompt([
+	{ 
+
+       }
+
+	]).then(answers => {
+
+    console.log(answers);
+});
+
+
+//console.log(keys);
+
+var argOne = process.argv[2];
+//options:
+	//my-tweets
+	//spotify-this-song 
+	//movie-this
+var argTwo = process.argv[3];
+//options:
+	//'<song name here>'
+	//'<movie name here>'
+
+if (argOne === "my-tweets") {
+	//ajax call to display tweets
+
+} else if (argOne === "spotify-this-song") {
+
+	if (argTwo != null ) {
+		// ajax call to spotify with argTwo song title
+
+		// Artist(s)
+	 	//The song's name
+	 	//A preview link of the song from Spotify
+	 	//The album that the song is from
+
+	} else {
+		// ajax call to spotify with "The Sign" by Ace of Base
+	}
+	
+} else if (argOne === "movie-this") {
+
+	if (argTwo != null ) {
+		// ajax call to OMDB with argTwo movie title
+
+		// Title of the movie.
+		//Year the movie came out.
+		//IMDB Rating of the movie.
+		//Rotten Tomatoes Rating of the movie.
+		//Country where the movie was produced.
+		//Language of the movie.
+		//Plot of the movie.
+		//Actors in the movie.
+
+	} else {
+		//output data for the movie 'Mr. Nobody.'
+
+	}
+	
+
+}
