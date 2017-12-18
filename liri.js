82dee39a23940afa158b5410a14d587d48da52d1
Jihanna02 <jihanna02@gmail.com>
2017-12-18 18:59:31 -0500

    implemented twitter api

--- liri.js
+++ liri.js
@@ -1,67 +1,84 @@
 var keys = require('./keys.js');
-var inquirer = require('inquirer');
 
-var twitterAPI = require('node-twitter-api');
-
-inquirer.prompt([
-	{ 
-	type: 'list',
-      message: "What would you like to do?",
-      choices: ["my-tweets", "spotify-this-song", "movie-this"],
-      name: "command"
-       } 
-
-	]).then(answers => {
-
-    var argOne = answers.command;
- //    var argOne = process.argv[2];
-	// //options:
-	// 	//my-tweets
-	// 	//spotify-this-song 
-	// 	//movie-this
-	// var argTwo = process.argv[3];
-	// //options:
-	// 	//'<song name here>'
-	// 	//'<movie name here>'
+    var argOne = process.argv[2];
+	//options:
+		//my-tweets
+		//spotify-this-song 
+		//movie-this
+	var argTwo = process.argv[3];
+	//options:
+		//'<song name here>'
+		//'<movie name here>'
 
 	if (argOne === "my-tweets") {
 		//ajax call to display tweets
+		var twitterAPI = require('node-twitter-api');
+		var twitter = new twitterAPI({
+	    consumerKey: keys.consumer_key,
+	    consumerSecret: keys.consumer_secret,
+	    callback: 'http://www.google.com'
+		});
 
-	} else if (argOne === "spotify-this-song") {
+		twitter.search({'q': 'jihanna02', 'count':"20"},
+		    keys.access_token_key,
+		    keys.access_token_secret,
+		    function(error, data, response) {
+		        if (error) {
+		            console.log("error alert");
+		        } else {
 
-		if (argTwo != null ) {
-			// ajax call to spotify with argTwo song title
+		        	for(var i = 0; i < data.statuses.length; i++){
+		        		console.log(data.statuses[i].text); 
+		        	}
+		        }
+		    }
+		);
 
-			// Artist(s)
-		 	//The song's name
-		 	//A preview link of the song from Spotify
-		 	//The album that the song is from
+ 	} else if (argOne === "spotify-this-song") {
 
-		} else {
-			// ajax call to spotify with "The Sign" by Ace of Base
-		}
+ 		console.log("selected spotify-this-song (with value)");
+
+		// if (argTwo != undefined ) {
+		// 	// ajax call to spotify with argTwo song title
+
+		// 	// Artist(s)
+		//  	//The song's name
+		//  	//A preview link of the song from Spotify
+		//  	//The album that the song is from
+
+		//  	console.log("selected spotify-this-song (with value)");
+
+		// } else {
+		// 	// ajax call to spotify with "The Sign" by Ace of Base
+		// 	console.log("selected spotify-this-song (undefined)");
+		// }
 		
 	} else if (argOne === "movie-this") {
 
-		if (argTwo != null ) {
-			// ajax call to OMDB with argTwo movie title
+		console.log("selected movie-this (undefined)");
 
-			// Title of the movie.
-			//Year the movie came out.
-			//IMDB Rating of the movie.
-			//Rotten Tomatoes Rating of the movie.
-			//Country where the movie was produced.
-			//Language of the movie.
-			//Plot of the movie.
-			//Actors in the movie.
+		// if (argTwo != undefined ) {
+		// 	// ajax call to OMDB with argTwo movie title
 
-		} else {
-			//output data for the movie 'Mr. Nobody.'
+		// 	// Title of the movie.
+		// 	//Year the movie came out.
+		// 	//IMDB Rating of the movie.
+		// 	//Rotten Tomatoes Rating of the movie.
+		// 	//Country where the movie was produced.
+		// 	//Language of the movie.
+		// 	//Plot of the movie.
+		// 	//Actors in the movie.
 
-		}
+		// 	console.log("selected movie-this (with value)");
+
+		// } else {
+		// 	//output data for the movie 'Mr. Nobody.'
+
+		// 	console.log("selected movie-this (undefined)");
+		// }
 		
 
 	}
 
 
-});
+// };
